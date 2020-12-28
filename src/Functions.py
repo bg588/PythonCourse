c2764eaa61f71edff5305812c5fb28a92f01c0eb
Ben <bc_gough@hotmail.com>
2020-12-28 16:17:03 +0000

    Formatting fixes

--- src/Functions.py
+++ src/Functions.py
@@ -1,14 +1,15 @@
-def testFunction():
+def function():
     print("This is a function running!")
 
-def run10Times():
+
+def run10times():
     count = 0
-    while count < 10 :
+    while count < 10:
         print('Execution number ' + str(count))
-        testFunction()
+        function()
         count += 1
 
-print('Hello, running some functions')
-testFunction()
-run10Times()
 
+print('Hello, running some functions')
+function()
+run10times()
