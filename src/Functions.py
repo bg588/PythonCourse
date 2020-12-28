519006165c04eb0c6b22b9e8552cc21a8135d5ee
Ben <bc_gough@hotmail.com>
2020-12-28 16:19:26 +0000

    Blast off added

--- src/Functions.py
+++ src/Functions.py
@@ -10,6 +10,16 @@ def run10times():
         count += 1
 
 
+def countdown():
+    n = 5
+    while n > 0:
+        print(n)
+        n -= 1
+    print('Blast off!')
+
+
 print('Hello, running some functions')
 function()
 run10times()
+print('Launch a rocket')
+countdown()
