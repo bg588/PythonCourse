8e1b188f38f5a91fc1fc7216a17537cbe6cfc1a0
Ben <bc_gough@hotmail.com>
2020-12-29 14:52:40 +0000

    Loops

--- src/Functions.py
+++ src/Functions.py
@@ -10,6 +10,20 @@ def run10times():
         count += 1
 
 
+def breaktest():
+    x = 0
+    while x < 10:
+        x += 1
+        if x == 2:
+            # continue causes us to go back to top of loop, so 2 won't print out
+            continue
+        print(x)
+        if x == 7:
+            # break causes us to leave the while totally
+            break
+    print("Now here as we left the loop at ", x)
+
+
 def countdown():
     n = 5
     while n > 0:
@@ -17,12 +31,17 @@ def countdown():
         n -= 1
     print('Blast off!')
 
+
 def givemeab():
     # note lack of type - not like java
     return "B"
 
-print("Give me a B!", givemeab())
 
+print("Give me a B!", givemeab())
+print()
+print("Break test : ")
+breaktest()
+print()
 print('Hello, running some functions')
 function()
 run10times()
